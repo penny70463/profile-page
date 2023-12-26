ce539e1f9654efe0cbf59b39485aca91fc6ef935
penny70463 <penny70463@gmail.com>
2023-12-26 16:15:35 +0800

    [fix] deploy failed on netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,4 +36,14 @@ export default defineNuxtConfig({
       Inter: [400, 500, 600, 700, 800, 900],
     },
   },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+     cssnano:
+       process.env.NODE_ENV === 'production'
+         ? { preset: ['default', { discardComments: { removeAll: true } }] }
+         : false, // disable cssnano when not in production
+    },
+ }
 });
